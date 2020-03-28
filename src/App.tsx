2195bd5fe5386a7462a26af256b4390edbedd031
Leon Erath <leon-erath@hotmail.de>
2020-03-28 11:41:27 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = io("http://localhost:3001");
+
+export type Status = "waiting" | "active" | "completed" | "rejected";
+
+interface Job {
+	firstname: string;
+	lastname: string;
+	email: string;
+	time: number;
+	jitsi: string;
+	status: Status;
 }
+interface JobInfo extends Job {
+	position: number;
+}
+
+const App = () => {
+	const [email, setEmail] = useState("");
+	const [isLoggedIn, setLoggedIn] = useState(false);
+	const [jobInfo, setJobInfo] = useState<JobInfo | null>(null);
+
+	useEffect(() => {
+		socket.on("login", (data: { success: boolean; jobInfo?: JobInfo }) => {
+			setLoggedIn(data.success);
+			if (data.jobInfo) {
+				setJobInfo(data.jobInfo);
+			}
+		});
+	}, [isLoggedIn]);
+
+	const handleLogin = () => {
+		console.log(email);
+		socket.emit("login", { email });
+	};
+
+	const renderLogin = () => {
+		return (
+			<>
+				<input
+					type="email"
+					value={email}
+					placeholder="Write your e-mail.."
+					onChange={e => setEmail(e.target.value)}
+				/>
+				<button onClick={handleLogin}>login</button>
+			</>
+		);
+	};
+
+	const renderJobInfo = () => {
+		if (!jobInfo) {
+			return;
+		}
+
+		return (
+			<div>
+				<div>{jobInfo.firstname}</div>
+				<div>{jobInfo.lastname}</div>
+				<div>{jobInfo.email}</div>
+				<div>{jobInfo.position}</div>
+				<div>{jobInfo.status}</div>
+			</div>
+		);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				{isLoggedIn ? renderJobInfo() : renderLogin()}
+			</header>
+		</div>
+	);
+};
 
 export default App;
