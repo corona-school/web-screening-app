26e292453ed702c9d0dbc5d1fe2f8a9a45f26197
Leon Erath <leon-erath@hotmail.de>
2020-03-29 17:21:37 +0200

    language

--- src/language/JobLanguage.tsx
+++ src/language/JobLanguage.tsx
@@ -1,18 +1,15 @@
 const TextMap = new Map([
-	["waiting", "Du wirst bald Verifiziert..."],
+	["waiting", "Du wirst bald verifiziert..."],
 	[
 		"active",
 		"Ein Mitarbeiter hat jetzt Zeit für Dich. Klick auf dem Link um die Verifizierung zu starten."
 	],
-	[
-		"completed",
-		"Ein Mitarbeiter hat jetzt Zeit für Dich. Klick auf dem Link um die Verifizierung zu starten."
-	],
+	["completed", "Herzlichgen Glückwunsch! Du wurdest erfolgreich verifiziert!"],
 	["rejected", "Es tut uns leid. Du konntest nicht verifiziert werden."]
 ]);
 
 const HeadlineMap = new Map([
-	["waiting", "Warteschlange"],
+	["waiting", "Einen Moment noch..."],
 	["active", "Los Gehts!"],
 	["completed", "Viel Spaß!"],
 	["rejected", "Ooops.."]
