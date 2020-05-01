ff866ff7147a303bbe1af002fdb228b341296d42
Leon Erath <leon-erath@hotmail.de>
2020-05-01 17:57:10 +0200

    feat: added translations for i18n

---
+++ src/language/translations.ts
@@ -0,0 +1,96 @@
+export const translation = {
+	JOB_STATUS_WAITING_DESCRIPTION: {
+		en:
+			"You will soon be getting to know each other, please stay on this page. If you want you can prepare yourself and join the video call.",
+		de:
+			"Du bist bald mit deinem Kennenlerngespräch dran, bleibe bitte auf dieser Seite. Wenn du willst kannst du dich schon vorbereiten und dem Video-Call beitreten.",
+	},
+	JOB_STATUS_ACTIVE_DESCRIPTION: {
+		en:
+			"An employee now has time for you. Click on the link to start the introductory conversation.",
+		de:
+			"Ein Mitarbeiter*in hat jetzt Zeit für Dich. Klick auf dem Link um das Kennenlerngespräch zu starten.",
+	},
+	JOB_STATUS_COMPLETED_DESCRIPTION: {
+		en: "Congratulations! You have been successfully verified!",
+		de: "Herzlichen Glückwunsch! Du wurdest erfolgreich verifiziert!",
+	},
+	JOB_STATUS_REJECTED_DESCRIPTION: {
+		en: "We are sorry. You could not be verified.",
+		de: "Es tut uns leid. Du konntest nicht verifiziert werden.",
+	},
+	JOB_STATUS_WAITING_HEADLINE: {
+		en: "One moment please...",
+		de: "Einen Moment noch...",
+	},
+	JOB_STATUS_ACTIVE_HEADLINE: {
+		en: "Lets go!",
+		de: "Los Geht's!",
+	},
+	JOB_STATUS_COMPLETED_HEADLINE: {
+		en: "Have fun!",
+		de: "Viel Spaß!",
+	},
+	JOB_STATUS_REJECTED_HEADLINE: {
+		en: "Ooops..",
+		de: "Ooops..",
+	},
+	WEBSITE_HEADER_TITLE: {
+		en: "Corona School",
+		de: "Corona School",
+	},
+	LOGIN_FORM_HEADLINE: {
+		en: "Welcome",
+		de: "Wilkommen",
+	},
+	LOGIN_FORM_DESCRIPTION: {
+		en:
+			"Here you can get verified as a student. We are here for you from <b> 9:00 a.m. - 12:00 p.m. and from 3:00 p.m. - 6:00 p.m. </b>.",
+		de:
+			"Hier kannst Du Dich als Student*in verifizieren lassen. Wir sind heute von <b>09:00 - 12:00 sowie von 15:00 - 18:00 Uhr</b> für Dich da.",
+	},
+	LOGIN_FORM_EMAIL_PLACEHOLDER: {
+		en: "Enter your email here ..",
+		de: "Trage hier deine E-Mail ein..",
+	},
+	LOGIN_FORM_BUTTON: {
+		en: "Start verification",
+		de: "Starte Verifizierung",
+	},
+	QUEUE_ABOUT: {
+		en: "Abort",
+		de: "Abbrechen",
+	},
+	QUEUE_LINK_VIDEO_CALL: {
+		en: "Link to the video call",
+		de: "Link zum Video-Call",
+	},
+	QUEUE_META_HEADER_WAITING: {
+		en: "Corona School | Waiting",
+		de: "Corona School | Warteschlange",
+	},
+	QUEUE_META_HEADER_ACTIVE: {
+		en: "Corona School | Meeting",
+		de: "Corona School | Kennenlerngespräch",
+	},
+	QUEUE_META_HEADER_COMPLETED: {
+		en: "Corona School | Done",
+		de: "Corona School | Fertig",
+	},
+	QUEUE_META_HEADER_REJECTED: {
+		en: "Corona School | Rejected",
+		de: "Corona School | Abgelehnt",
+	},
+	QUEUE_ACTIVE_MODAL_HEADLINE: {
+		en: "It's your turn!",
+		de: "Du bist dran!",
+	},
+	QUEUE_ACTIVE_MODAL_DESCRIPTION: {
+		en: "is waiting for you!",
+		de: "wartet schon auf dich!",
+	},
+	QUEUE_ACTIVE_MODAL_LINK: {
+		en: "Start a video call",
+		de: "Videoanruf starten",
+	},
+};
