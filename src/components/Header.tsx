c1a0bed2b555c7cf176a9182a479381b8a69d224
Leon Erath <leon-erath@hotmail.de>
2020-05-10 17:39:32 +0200

    fix: somewhat fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import logo from "../icons/logo.svg";
 import "./Header.scss";
+import { Typography } from "antd";
+
+const { Title } = Typography;
 
 const Header = () => {
 	return (
 		<div className="header">
 			<div>
 				<img src={logo} className="logo" alt="Corona School Logo"></img>
-				<span className="logo-title">Corona School</span>
+
+				<Title
+					level={2}
+					style={{
+						color: "white",
+						fontWeight: "bold",
+						margin: "0 0 0 12px",
+					}}>
+					Corona School
+				</Title>
 			</div>
 		</div>
 	);
